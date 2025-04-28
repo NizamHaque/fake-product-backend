35d7aa5cd13aaa31ca4d3e9f49694885714e5408
NizamHaque <mdnizamhaque13@gmail.com>
2025-04-29 01:53:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,8 +66,8 @@ def submit_review():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=True)
 # import os
 
 # if __name__ == "__main__":
